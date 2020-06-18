073c9b79830cd316d3114b2f57ec5cf19b8d0f68
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-06-18 19:49:57 -0400

    graphiql working

--- app/graphql/types/artwork_type.rb
+++ app/graphql/types/artwork_type.rb
@@ -1,13 +1,13 @@
 module Types
   class ArtworkType < Types::BaseObject
     field :id, ID, null: false
-    field :title, String, null: true
+    field :title, String, null: false
     field :year, Integer, null: true
     field :medium, String, null: true
     field :notes, String, null: true
     field :location, String, null: true
     field :image_loc, String, null: true
-    field :artist_id, Integer, null: true
+    field :artist_id, Integer, null: false
     field :exhibition_id, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,16 @@
 #   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 # end
 
+# Rails.application.routes.draw do
+#   if Rails.env.development?
+#     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+#   end
+#   post "/graphql", to: "graphql#execute"
+# end
+
+# above commented out, trying something else per https://medium.com/@DrawandCode/building-a-graphql-api-in-rails-part-start-coding-8b1de6d75041
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
-  end
-  post "/graphql", to: "graphql#execute"
+  # ...
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  # ...
 end
